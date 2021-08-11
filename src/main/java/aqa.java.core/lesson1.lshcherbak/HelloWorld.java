e7fb0f6a673ecbae4e17907c260595822edf7718
Laura Shcherbak <laura.shcherbak@gmail.com>
2021-08-11 11:30:30 +0300

    [Lesson1] HW1 added

---
+++ src/main/java/aqa.java.core/lesson1.lshcherbak/HelloWorld.java
@@ -0,0 +1,9 @@
+package main.java.aqa.java.core.lesson1.lshcherbak;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World.\nI'm Laura Shcherbak.");
+    }
+    
+}
