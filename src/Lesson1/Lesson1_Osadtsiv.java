d4b12407bb95f58ff72b1c52488a0aab079ab335
OsadtsivMykhailo <osadtsiv.m@gmail.com>
2021-08-11 12:53:44 +0300

    Check HomeWork1 please

--- src/Lesson1/Lesson1_Osadtsiv.java
+++
@@ -1,7 +0,0 @@
-package Lesson1;
-
-public class Lesson1_Osadtsiv {
-    public static void main(String[] args) {
-        System.out.println("Hello world, are you ready to be conquered?");
-    }
-}
