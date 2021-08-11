a59547c2d2c846a0da879b8a048f48732f44ebcf
yuliamykhalchuk <yulianyzhnyk999@gmail.com>
2021-08-11 23:42:18 +0300

    add my first program

--- src/lesson1/MykhalchukYulia/HelloJava.java
+++
@@ -1,7 +0,0 @@
-package lesson1.MykhalchukYulia;
-
-public class HelloJava {
-    public static void main(String[] args) {
-        System.out.println("Hello beautiful Java");
-    }
-}
---
+++ src/task1/MykhalchukYulia/HelloJava.java
@@ -0,0 +1,7 @@
+package task1.MykhalchukYulia;
+
+public class HelloJava {
+        public static void main(String[] args) {
+            System.out.println("Hello beautiful Java");
+        }
+}
