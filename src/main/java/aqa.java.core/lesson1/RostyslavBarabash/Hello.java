d322ffc5b0651ca21ee852fdfe57d75f14a88253
rostyslavbas <rostyslavbas@gmail.com>
2021-08-11 14:02:54 +0300

    rostyslav barabash commit

---
+++ src/main/java/aqa.java.core/lesson1/RostyslavBarabash/Hello.java
@@ -0,0 +1,8 @@
+package main.java.aqa.java.core.lesson1.rostyslavbas;
+
+public class HelloWorld {
+    public static void main(String[]args){
+
+        System.out.print("Hello world! I`m Rostyslav Barabash");
+    }
+}
