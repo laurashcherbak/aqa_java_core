d914022c892876e5d1757d9adfa930cfad8cdd89
KrychkovskaVira <88768615+KrychkovskaVira@users.noreply.github.com>
2021-08-11 19:04:25 +0300

    java

---
+++ src/main/java/aqa.java.core/lesson1/ViraKrychkovska/HelloWorld.java
@@ -0,0 +1,6 @@
+package main.java.aqa.java.core.lesson1.ViraKrychkovska;
+public class HelloWorld {
+    public static void main(String[]args){
+        System.out.println("Hello world! I'm Vira Krychkovska");
+    }
+}
