13726641bdacde5d19493a96cf1b5943148a2be4
yuliamykhalchuk <yulianyzhnyk999@gmail.com>
2021-08-11 22:34:02 +0300

    add my first pr

---
+++ src/main/java/aqa.java.core/lesson1/MykhalchukYulia/HelloJava.java
@@ -0,0 +1,8 @@
+package main.java.aqa.java.core.lesson1.MykhalchukYulia;
+
+public class HelloJava{
+    public static void main(String[]args){
+        System.out.println("Hello beautiful Java");
+     }
+
+}
