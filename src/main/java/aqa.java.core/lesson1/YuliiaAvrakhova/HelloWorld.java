5423d670e9774026dfc844cc7f94c3f64da91556
Ivan Avrakhov <ivanavrahov@gmail.com>
2021-08-11 18:24:27 +0300

    first commit

---
+++ src/main/java/aqa.java.core/lesson1/YuliiaAvrakhova/HelloWorld.java
@@ -0,0 +1,8 @@
+package main.java.aqa.java.core.lesson1.yuliiaavrakhova;
+
+public class HelloWorld {
+    public static void main(String[]args){
+
+        System.out.print("Hello world! I`m Yuliia Avrakhova");
+    }
+}
