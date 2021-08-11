c8522d60c9370bd41821b6089eb636c95d56d113
Yulia Gnatyuk <yliagnatyuk@gmail.com>
2021-08-11 23:16:16 +0300

    add home work from Yulia Gnatyuk, lesson1

---
+++ src/main/java/aqa.java.core/lesson1/YliiaGnatuik/HelloWorldYuliaG.java
@@ -0,0 +1,9 @@
+package aqa.java.core.lesson1.YliiaGnatuik;
+
+public class HelloWorldYuliaG {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World, I am Yliia Gnatuik");
+    }
+
+}
