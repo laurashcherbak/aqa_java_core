48fa955213bce86e575d58d9e60133cdb2271e13
Ivan Avrakhov <ivanavrahov@gmail.com>
2021-08-11 16:57:05 +0300

    Update File Avrakhova

--- src/main/java/aqa.java.core/lesson1/lesson1_Avrakhova/Main.java
+++ src/main/java/aqa.java.core/lesson1/lesson1_Avrakhova/Main.java
@@ -1,6 +1,6 @@
 package lesson1_Avrakhova;
 
-public class Main {
+public class Main{
     public static void main(String[] args) {
         System.out.println("Hello world! I am Yuliia Avrakhova");
     }
