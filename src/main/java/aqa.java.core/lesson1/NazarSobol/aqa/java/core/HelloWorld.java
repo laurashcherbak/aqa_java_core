be8fe6cb7b078d17bd7cce0c2fbc2f3c26c98ff7
Nazar <sobol.nazar@gmail.com>
2021-08-11 17:40:34 +0300

    implemented class {main}

---
+++ src/main/java/aqa.java.core/lesson1/NazarSobol/aqa/java/core/HelloWorld.java
@@ -0,0 +1,9 @@
+package aqa.java.core;
+
+        public class HelloWorld {
+                public static void main(String[]args){
+                        System.out.println("Hello world I am Nazar Sobol");
+
+
+        }
+        }
