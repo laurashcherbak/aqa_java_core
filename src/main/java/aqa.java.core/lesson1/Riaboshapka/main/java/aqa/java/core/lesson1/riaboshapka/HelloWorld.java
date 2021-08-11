a6134fd04b693eb56b86c9eaeddcfe3f243da3cd
Vita <viktorya.ukr@gmail.com>
2021-08-11 21:33:20 +0300

    Create HelloWorld class

---
+++ src/main/java/aqa.java.core/lesson1/Riaboshapka/main/java/aqa/java/core/lesson1/riaboshapka/HelloWorld.java
@@ -0,0 +1,10 @@
+package main.java.aqa.java.core.lesson1.riaboshapka;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World! I am Victoria Riaboshapka");
+    }
+
+}
