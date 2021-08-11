8be9179ff520d4f2cde4b99dc17f02d38f19e05c
Liudmila <lyuda.jazz@gmail.com>
2021-08-12 00:35:34 +0300

    homework1

---
+++ src/lesson1/liudmilakapusniak/Homework1Kapusniak.java
@@ -0,0 +1,7 @@
+package lesson1.liudmilakapusniak;
+
+public class Homework1Kapusniak {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! I am Liuda Kapusniak");
+    }
+}
