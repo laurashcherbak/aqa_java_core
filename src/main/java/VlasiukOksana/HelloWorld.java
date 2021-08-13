c09dc04492848d36c52dde5ec6f0405935d54c76
Vlasiuk Oksana <ksenia.vlasiuk7@gmail.com>
2021-08-13 17:38:25 +0300

    Check my Homework1, please)

--- src/main/java/VlasiukOksana/HelloWorld.java
+++ src/main/java/VlasiukOksana/HelloWorld.java
@@ -1,8 +1,5 @@
 package VlasiukOksana;
 
 public class HelloWorld {
-    public static void main(String[]args){
-
-        System.out.println("Hello World!I am Oksana Vlasiuk");
-    }
+    public static void main(String[]args){System.out.println("Hello World!I am Oksana Vlasiuk");}
 }
