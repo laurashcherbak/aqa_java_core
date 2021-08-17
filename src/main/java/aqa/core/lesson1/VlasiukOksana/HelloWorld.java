40bcfcdc876b07358c870108fab450e1f7df2d53
andrii.kylo <andrii.kylo@groupm.tech>
2021-08-17 10:20:49 +0300

    replaced homework into correct location

--- src/main/java/aqa/core/lesson1/VlasiukOksana/HelloWorld.java
+++ src/main/java/aqa/core/lesson1/VlasiukOksana/HelloWorld.java
@@ -1,4 +1,4 @@
-package VlasiukOksana;
+package aqa.core.lesson1.VlasiukOksana;
 
 public class HelloWorld {
     public static void main(String[]args){System.out.println("Hello World!I am Oksana Vlasiuk");}
