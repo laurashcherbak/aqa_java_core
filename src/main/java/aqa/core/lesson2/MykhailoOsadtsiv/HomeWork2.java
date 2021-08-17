5f9122442f9cca67c46b5593017df64f0859802d
OsadtsivMykhailo <osadtsiv.m@gmail.com>
2021-08-17 22:13:07 +0300

    HomeWork2

--- src/main/java/aqa/core/lesson2/MykhailoOsadtsiv/HomeWork2.java
+++ src/main/java/aqa/core/lesson2/MykhailoOsadtsiv/HomeWork2.java
@@ -1,7 +1,19 @@
-package main.java.aqa.core.lesson2.MykhailoOsadtsiv;
+package aqa.core.lesson2.MykhailoOsadtsiv;
 
 public class HomeWork2 {
     public static void main(String[] args) {
-        //todo tomorrow
+        System.out.println("Hello human!");
+        byte b1 = 123;
+        short s2 = 124;
+        int i3 = 125;
+        long l4 =126;
+        float f5 = 127f;
+        double d6 = 128;
+        char c7 = 'ü';
+        boolean b8 = false;
+        System.out.println(b1 + " " + s2 + " " + i3 + " " + l4 + " " + f5 + " " + d6 + " " +
+                c7 + " " +b8);
+        System.out.println("sum of 2 digits" + ": i3+l4=" + (i3+l4));
     }
+
 }
