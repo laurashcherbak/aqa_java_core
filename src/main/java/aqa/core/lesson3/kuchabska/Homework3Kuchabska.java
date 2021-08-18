ffa5fed2154257fb88ace569a357bec3f28ac462
Halyna <halyna.kuchabska@gmail.com>
2021-08-18 23:18:50 +0300

    update file name

--- src/main/java/aqa/core/lesson3/kuchabska/Homework3Kuchabska.java
+++ src/main/java/aqa/core/lesson3/kuchabska/Homework3Kuchabska.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Objects;
 
-public class homework3_kuchabska {
+public class Homework3Kuchabska {
 
     public static void main(String[] args) throws IOException {
         //1
