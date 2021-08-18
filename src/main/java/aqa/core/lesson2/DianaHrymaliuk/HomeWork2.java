d4755e80ce4d7a151675a265e8a2f7157d02bc38
DianaHrymaliuk <diana.hrymaliuk@gmail.com>
2021-08-18 11:17:16 +0300

    HW2

--- src/main/java/aqa/core/lesson2/DianaHrymaliuk/HomeWork2.java
+++ src/main/java/aqa/core/lesson2/DianaHrymaliuk/HomeWork2.java
@@ -1,4 +1,12 @@
 package aqa.core.lesson2.DianaHrymaliuk;
 
+
 public class HomeWork2 {
+//    Task 1.
+    public static void main(String[]arg){
+        System.out.println("Hello human!");
+    }
+
+//    Task 2.
+
 }
