23924d26b1454a266047da34385f31f697ce868d
DianaHrymaliuk <diana.hrymaliuk@gmail.com>
2021-08-18 16:27:08 +0300

    HomeWork2

--- src/main/java/aqa/core/lesson1/HalynaKuchabska/Homework1Kuchabska.java
+++
@@ -1,7 +0,0 @@
-package aqa.core.lesson1.HalynaKuchabska;
-
-public class Homework1Kuchabska {
-    public static void main(String[] args) {
-        System.out.println("Hello World! I am Halyna Kuchabska");
-    }
-}
--- src/main/java/aqa/core/lesson2/DianaHrymaliuk/HomeWork2.java
+++ src/main/java/aqa/core/lesson2/DianaHrymaliuk/HomeWork2.java
@@ -8,69 +8,53 @@ import static java.lang.Math.sqrt;
 
 public class HomeWork2 {
 
-//Всі таски окремо працюють
+
 //    Task 1.
-    public static void main(String[]arg) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello human!");
-    }
 
 ////    Task 2-3.
-    private static
-        byte b1 = 25;
-    private static
+
+        byte b = 25;
         short s2 = 0b1001;
-    private static
         int i = 0x2f;
-    private static
         long l = 2345678923456L;
-    private static
         float f = 18.456F;
-    private static
         double d = 77.234;
-    private static
         char c1 = 'h';
-    private static
         boolean bl = true;
 
 //    //    Task 4.
-    public static void main(String[] args) {
-        assignOperators();
-        System.out.println(b1);
-        System.out.println(s2);
-        System.out.println(i);
-        System.out.println(l);
-        System.out.println(f);
-        System.out.println(d);
-        System.out.println(c1);
-        System.out.println(bl);
+        System.out.print(b + "\n");
+        System.out.print(s2 + "\n");
+        System.out.print(i + "\n");
+        System.out.print(l + "\n");
+        System.out.print(f + "\n");
+        System.out.print(d + "\n");
+        System.out.print(c1 + "\n");
+        System.out.print(bl + "\n");
 
 ////        Task 5.
-        System.out.println(b1 + f);
-    }
+        System.out.print(b + f + "\n");
+
 
 //    Task 6.
 
-            public static void main(String[] args) throws IOException {
+
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             System.out.println("Enter Integer:");
-            int i = Integer.parseInt(br.readLine());
-            System.out.println(i * 2);
-        }
+            int i1 = Integer.parseInt(br.readLine());
+            System.out.println(i1 * 2);
+
     //    Task 7.
-    public static void main(String[]arg) {
-        assignOperators();
-        double a = (14./209 + 14 * (29-13*13 + (14./3)));
-        System.out.println(a);
-    }
+
+        double a1 = (14./209 + 14 * (29-13*13 + (14./3)));
+        System.out.println(a1);
 
     //    Task 8.
-    public static void main(String[] arg) {
-        assignOperators();
-        double a = (5 + 2*3*3)/sqrt(7.*4*4/(5 + 2*2));
-        System.out.println(a);
 
-    }
+        double a2 = (5 + 2*3*3)/sqrt(7.*4*4/(5 + 2*2));
+        System.out.println(a2);
 
-    private static void assignOperators() {
     }
 }
