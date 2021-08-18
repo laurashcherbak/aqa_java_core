884a909a11b5f3647d0d0d34009f8c1380d28aef
Назар <hakernaz@gmail.com>
2021-08-18 14:52:49 +0300

    Final Udate code Run every Task- Done! Succsess!

--- src/main/java/aqa/core/lesson2/vdovych/HomeWork2.java
+++ src/main/java/aqa/core/lesson2/vdovych/HomeWork2.java
@@ -1,15 +1,14 @@
 package main.java.aqa.core.lesson2.vdovych;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class HomeWork2 {
-// Task1
 
-    /* public static void main(String[] args) {
+    // Task1
+
+    /*
+     public static void main(String[] args) {
         System.out.println("Hello human!");
-    } */
+    }
+    */
 
 
     //Task 2 - 5
@@ -22,19 +21,21 @@ public static void main(String[] args) {
     long d = 1121L;
     float e = 3.14F;
     double f = 5.5;
-    char g = 'Nazar';
+    String g = "Nazar";
     boolean l = true;
 
-    System.out.printf("Result =" + a,"\n");
-    System.out.printf("Result =" + b,"\n");
-    System.out.printf("Result =" + c,"\n");
-    System.out.printf("Result =" + d,"\n");
-    System.out.printf("Result =" + e,"\n");
-    System.out.printf("Result =" + f,"\n");
-    System.out.printf("Result =" + g,"\n");
-    System.out.printf("Result =" + l,"\n");
+    System.out.printf("Result =" + a + "\n");
+    System.out.printf("Result =" + b + "\n");
+    System.out.printf("Result =" + c + "\n");
+    System.out.printf("Result =" + d + "\n");
+    System.out.printf("Result =" + e + "\n");
+    System.out.printf("Result =" + f + "\n");
+    System.out.printf("Result =" + g + "\n");
+    System.out.printf("Result =" + l + "\n");
 
-    //System.out.println("Result sum =" + a + b);
+    int Suma = a + b;
+
+    System.out.println("Result sum =" + Suma);
     }
     */
     //Task 6
@@ -49,11 +50,17 @@ public static void main(String[] args) {
 
 
     //Task 7
-    /*double a = (14.0 / 209.0) + 14 * ((29 - 13 * 13) + (14.0 / 3.0));
-        System.out.println("Result =" + a); */
+    /*
+    public static void main(String[] args) {
+    double a = (14.0 / 209.0) + 14 * ((29 - 13 * 13) + (14.0 / 3.0));
+        System.out.println("Result =" + a); }
+        */
 
 
     //Task 8
-    /*double b = (5 + (2 * 3 * 3)) / (Math.sqrt(7 * ((4 * 4) / (5 + (2 * 2)))));
-    System.out.println("Result =" + b);*/
+    /*public static void main(String[] args) {
+
+    double b = (5 + (2 * 3 * 3)) / (Math.sqrt(7 * ((4 * 4) / (5 + (2 * 2)))));
+    System.out.println("Result =" + b);
+    }*/
 }
