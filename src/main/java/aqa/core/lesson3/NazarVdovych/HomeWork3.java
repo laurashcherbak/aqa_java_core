7aca3b5531d43755b225b9ec96b83b929f5e847e
Назар <hakernaz@gmail.com>
2021-08-18 21:30:23 +0300

    Task 1 - Done!

---
+++ src/main/java/aqa/core/lesson3/NazarVdovych/HomeWork3.java
@@ -0,0 +1,14 @@
+package aqa.core.lesson3.NazarVdovych;
+
+public class HomeWork3 {
+     public static void main(String[] args) {
+         int k;
+            for(k = 0; k <= 20; k++){
+            if (k % 2 != 0) {
+                System.out.print(k + "\n");
+            }
+
+         }
+         System.out.print("Програма вивела не парні числа від 0 до 20"+"\n");
+    }
+}
