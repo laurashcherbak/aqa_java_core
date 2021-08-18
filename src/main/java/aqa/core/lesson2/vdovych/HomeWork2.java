f25384d719814b0550df3b5e42f62f86b6ec5549
Назар <hakernaz@gmail.com>
2021-08-18 12:12:17 +0300

    All Task - done!

--- src/main/java/aqa/core/lesson2/vdovych/HomeWork2.java
+++ src/main/java/aqa/core/lesson2/vdovych/HomeWork2.java
@@ -1,7 +1,55 @@
 package main.java.aqa.core.lesson2.vdovych;
 
 public class HomeWork2 {
-    public static void main(String[] args) {
+// Task1
+
+    /* public static void main(String[] args) {
         System.out.println("Hello human!");
+    } */
+
+
+    //Task 2 - 5
+    /*
+public static void main(String[] args) {
+
+    byte a = 10;
+    short b = 20;
+    int c = 120;
+    long d = 1121L;
+    float e = 3.14F;
+    double f = 5.5;
+    char g = 'Nazar';
+    boolean l = true;
+
+    System.out.printf("Result ="a,"\n");
+    System.out.printf("Result ="b,"\n");
+    System.out.printf("Result ="c,"\n");
+    System.out.printf("Result ="d,"\n");
+    System.out.printf("Result ="e,"\n");
+    System.out.printf("Result ="f,"\n");
+    System.out.printf("Result ="g,"\n");
+    System.out.printf("Result ="l,"\n");
+
+    //System.out.println("Result sum ="a + b);
     }
+    */
+    //Task 6
+    /*public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter Integer:");
+        int anynumber = Integer.parseInt(br.readLine());
+        System.out.println("Result =" anynumber * 2);
+
+ }
+
+    }*/
+
+    //Task 7
+    /*double a = (14.0 / 209.0) + 14 * ((29 - 13 * 13) + (14.0 / 3.0));
+        System.out.println("Result ="a); */
+
+
+    //Task 8
+    /*double b = (5 + (2 * 3 * 3)) / (Math.sqrt(7 * ((4 * 4) / (5 + (2 * 2)))));
+    System.out.println("Result ="b);*/
 }
