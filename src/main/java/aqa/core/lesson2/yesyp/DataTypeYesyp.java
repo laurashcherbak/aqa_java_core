5a552c31de97e2afadb32b34707e2d0a5bbf9c08
olich <olichka007@gmail.com>
2021-08-18 17:29:04 +0300

    HomeWork2 data types and operators

---
+++ src/main/java/aqa/core/lesson2/yesyp/DataTypeYesyp.java
@@ -0,0 +1,28 @@
+package main.java.aqa.core.lesson2.yesyp;
+
+public class DataTypeYesyp {
+    public static void main(String[] args) {
+        byte b = 10;
+        short s = 10_000;
+        int i = 100_000;
+        long l = 1_000_000_000L;
+        float f = 123.123F;
+        double d = 555.55;
+        char c = 'm';
+        boolean bb = true;
+        String hi = "Hello human";
+        int sum = b+s;
+
+        System.out.println(hi);
+        System.out.println("b= " +b);
+        System.out.println("s= " +s);
+        System.out.println("i= " +i);
+        System.out.println("l= " +l);
+        System.out.println("f= " +f);
+        System.out.println("d= " +d);
+        System.out.println("c= " +c);
+        System.out.println("bb= " +bb);
+        System.out.println("Sum b+s= " +sum);
+
+    }
+}
---
+++ src/main/java/aqa/core/lesson2/yesyp/Expression1.java
@@ -0,0 +1,12 @@
+package main.java.aqa.core.lesson2.yesyp;
+
+
+public class Expression1 {
+    public static void main(String[] args) {
+        double x1 = 14/209.0+14*(29-(Math.pow(13,2))+14/3.0);
+        System.out.println("x1= " +x1);
+        double x2 = 5+2*(Math.pow(3,2))/(Math.sqrt(7*(Math.pow(4,2)/5+2*2)));
+        System.out.println("x2= " +x2);
+    }
+    }
+
