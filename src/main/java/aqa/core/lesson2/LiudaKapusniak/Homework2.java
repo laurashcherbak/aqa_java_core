1ee28f39b09092ca407f0bc3f8f93e88955f9e23
Liudmila <lyuda.jazz@gmail.com>
2021-08-18 20:43:32 +0300

    Part homework2 done

---
+++ src/main/java/aqa/core/lesson2/LiudaKapusniak/Homework2.java
@@ -0,0 +1,22 @@
+package aqa.core.lesson2.LiudaKapusniak;
+
+ public class Homework2 {
+     //Task1
+     public static void main(String[] args) {
+         System.out.println("Hello human");
+
+         //Task2
+
+
+             byte a5 = 10;
+             short b7 = 20;
+             int c9 = 30;
+             long d11 = 40;
+             float e13 = 50;
+             double f15 = 60;
+             char g17 = 70;
+             boolean h19 = true;
+             String s20 = "Hello human";
+         }
+     }
+
