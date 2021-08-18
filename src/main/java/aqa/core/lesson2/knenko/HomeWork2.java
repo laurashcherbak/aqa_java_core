4816bb6a68b5ec8ac49cd1dac3655b6861040372
ketrinb <ketrinb@ukr.net>
2021-08-18 12:55:55 +0300

    add 5 tasks

---
+++ src/main/java/aqa/core/lesson2/knenko/HomeWork2.java
@@ -0,0 +1,54 @@
+package knenko;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class HomeWork2 {
+    //task_1
+    public static void main(String[] args) {
+        System.out.println("Hello human!");
+
+    //task_2
+        byte b1;
+        short s2;
+        int i3;
+        long l4;
+        float f5;
+        double d6;
+        char c7;
+        boolean b8;
+        String s9;
+    //task_3
+        b1 = 100;
+        s2 = 0b10001;
+        i3 = 0x3f;
+        l4 = 222233334567L;
+        f5 = 19.838F;
+        d6 = 46.098;
+        c7 = 'a';
+        b8 = false;
+        s9 = "Hello Human";
+
+     //task_4
+        System.out.println("b1 = " + b1);
+        System.out.println("s2 = " + s2);
+        System.out.println("i3 = " + i3);
+        System.out.println("l4 = " + l4);
+        System.out.println("f5 = " + f5);
+        System.out.println("d6 = " + d6);
+        System.out.println("c7 = " + c7);
+        System.out.println("s9 = " + s9);
+
+
+   //task_5
+    int x = -5;
+    int y = 10;
+
+    System.out.println(x+y);
+
+
+
+}
+}
+
