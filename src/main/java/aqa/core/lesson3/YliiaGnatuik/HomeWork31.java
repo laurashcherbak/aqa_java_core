e132e2f55b78bbb3e0e620e25cda136a909d66de
Yulia Gnatyuk <yliagnatyuk@gmail.com>
2021-08-18 23:54:12 +0300

    add home work from Yulia Gnatyuk, lesson3

---
+++ src/main/java/aqa/core/lesson3/YliiaGnatuik/HomeWork31.java
@@ -0,0 +1,12 @@
+package aqa.core.lesson3.YliiaGnatuik;
+
+public class HomeWork31 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 20; i++) {
+            if ((i & 1) != 0) {
+                System.out.println (i);
+            }
+        }
+    }
+
+}
