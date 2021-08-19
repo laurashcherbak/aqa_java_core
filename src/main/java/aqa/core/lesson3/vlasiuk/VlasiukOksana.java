a64b524ffc6b0606237b398bd1e935e322f4c7f7
Vlasiuk Oksana <ksenia.vlasiuk7@gmail.com>
2021-08-19 17:42:24 +0300

    lesson 3 Vlasiuk Oksana

--- src/main/java/aqa/core/lesson3/vlasiuk/VlasiukOksana.java
+++ src/main/java/aqa/core/lesson3/vlasiuk/VlasiukOksana.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class VlasiukOksana {
     public static void main(String[] arg){
         int n = 20;
-        System.out.print("Odd Numbers from 1 to " + n + " are: ");
+        System.out.print("Odd numbers from 1 to " + n + " are: ");
         for (int i = 1; i <= n; i++) {
             if (i % 2 == 0) {
                 System.out.print(i + " ");
@@ -20,32 +20,32 @@ public class VlasiukOksana {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Please Enter The First Number");
+        System.out.println("Please enter the first number");
         num1 = input.nextInt();
 
-        System.out.println("Please Enter The Second Number");
+        System.out.println("Please enter the second number");
         num2 = input.nextInt();
 
         Scanner op = new Scanner (System.in);
 
-        System.out.println("Please Enter The Operation");
+        System.out.println("Please enter the operation");
         operation = op.next();
 
         if (operation.equals("+"))
         {
-            System.out.println("Your Answer is "+(num1 + num2));
+            System.out.println("Your answer is "+(num1 + num2));
         }
         else if (operation.equals("-"))
         {
-            System.out.println("Your Answer is "+(num1 - num2));
+            System.out.println("Your answer is "+(num1 - num2));
         }
         else if (operation.equals("*"))
         {
-            System.out.println("Your Answer is "+(num1 * num2));
+            System.out.println("Your Aanswer is "+(num1 * num2));
         }
         else if (operation.equals("/"))
         {
-            System.out.println("Your Answer is "+(num1 / num2));
+            System.out.println("Your answer is "+(num1 / num2));
         }
     }
 }
