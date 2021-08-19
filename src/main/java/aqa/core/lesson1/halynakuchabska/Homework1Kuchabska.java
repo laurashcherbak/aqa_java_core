275848be0e711ac0ab3b6022351fddcdb1271b77
Jegoush <l.jegoush@gmail.com>
2021-08-20 01:11:48 +0300

    Romanov_Yevgen_homework_4_array&linear

--- src/main/java/aqa/core/lesson1/halynakuchabska/Homework1Kuchabska.java
+++
@@ -1,7 +0,0 @@
-package aqa.core.lesson1.halynakuchabska;
-
-public class Homework1Kuchabska {
-    public static void main(String[] args) {
-        System.out.println("Hello World! I am Halyna Kuchabska");
-    }
-}
