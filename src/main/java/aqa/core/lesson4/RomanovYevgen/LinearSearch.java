9a0c67eea0175a85abb74925a7220ff7e302ce79
Jegoush <l.jegoush@gmail.com>
2021-08-20 01:24:35 +0300

    Romanov_Yevgen_homework_4_array&linear_n

---
+++ src/main/java/aqa/core/lesson4/RomanovYevgen/LinearSearch.java
@@ -0,0 +1,24 @@
+package aqa.core.lesson4.RomanovYevgen;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] value = {5, 10, 15, 18, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 91, 95, 100};
+        int searchedValue = 18;
+        int pos = 0;
+        boolean found = false;
+        while (pos < value.length && !found) {
+            if (value[pos] == searchedValue) {
+                found = true;
+
+            } else {
+                pos++;
+            }
+            if (found) {
+                System.out.println("You catch me:" + pos);
+            } else {
+                System.out.println("ha ha ha!)");
+            }
+
+        }
+    }
+}
