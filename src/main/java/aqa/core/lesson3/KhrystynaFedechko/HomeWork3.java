927bee65e3bad34886ea6ec016b72b19478be896
kfedechko <f530fa3927adc7263f3b2176090f07eb6f91cc36>
2021-08-19 16:49:39 +0300

    array fix

--- src/main/java/aqa/core/lesson3/KhrystynaFedechko/HomeWork3.java
+++ src/main/java/aqa/core/lesson3/KhrystynaFedechko/HomeWork3.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class HomeWork3 {
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         for (int i = 1; i < 20; i++) {
             if (i % 2 != 0) {
                 System.out.print(i + ", ");
