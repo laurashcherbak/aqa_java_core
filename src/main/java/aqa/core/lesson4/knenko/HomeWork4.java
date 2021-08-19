4085f68214f2f573c64a335d1e40a993c7508221
ketrinb <ketrinb@ukr.net>
2021-08-20 00:10:05 +0300

    my homework number 3

---
+++ src/main/java/aqa/core/lesson4/knenko/HomeWork4.java
@@ -0,0 +1,15 @@
+package aqa.core.lesson4.knenko;
+
+public class HomeWork4 {
+        public static void main(String[] args) {
+                int[] myArray = new int[100];
+                int x = 57;
+                for (int i = 0; i < myArray.length; i++) {
+                        myArray[i] = i + 1;
+                        if (myArray[i] == x) {
+                                System.out.println("Value:" + myArray[i] + ", index:" +i);
+                                break;
+                        }
+                }
+        }
+}
