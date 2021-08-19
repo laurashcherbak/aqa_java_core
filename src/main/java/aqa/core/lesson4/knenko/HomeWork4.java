1823ac3db822df37066202646feb9b8cdc85fbe7
ketrinb <ketrinb@ukr.net>
2021-08-20 00:42:43 +0300

    my homework number 4

--- src/main/java/aqa/core/lesson4/knenko/HomeWork4.java
+++ src/main/java/aqa/core/lesson4/knenko/HomeWork4.java
@@ -2,14 +2,37 @@ package aqa.core.lesson4.knenko;
 
 public class HomeWork4 {
         public static void main(String[] args) {
+
                 int[] myArray = new int[100];
                 int x = 57;
                 for (int i = 0; i < myArray.length; i++) {
                         myArray[i] = i + 1;
                         if (myArray[i] == x) {
-                                System.out.println("Value:" + myArray[i] + ", index:" +i );
+                                System.out.println("Value:" + myArray[i] + ", index:" + i);
                                 break;
+                        }
+                        //Unfortunately, the second task did not work.
+                        int firstIndex = 0;
+                        int lastIndex = myArray.length - 1;
+
+
+                        while (firstIndex != lastIndex) {
+                                int middleIndex = (firstIndex + lastIndex) / 2;
+
+                                if (myArray[middleIndex] > x) {
+                                        lastIndex = middleIndex - 1;
+
+                                } else if (myArray[middleIndex] < x) {
+                                        firstIndex = middleIndex + 1;
+
+                                } else if (myArray[middleIndex] == x) {
+                                        System.out.println(middleIndex);
+                                }
+
                         }
                 }
         }
 }
+
+
+
