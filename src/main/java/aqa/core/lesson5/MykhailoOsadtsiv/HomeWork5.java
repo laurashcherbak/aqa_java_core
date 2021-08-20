17f9485980e52625f5e07855ce3652ca8179bb09
OsadtsivMykhailo <osadtsiv.m@gmail.com>
2021-08-20 21:23:45 +0300

    HomeWork5, Task1 complete

---
+++ src/main/java/aqa/core/lesson5/MykhailoOsadtsiv/HomeWork5.java
@@ -0,0 +1,118 @@
+package aqa.core.lesson5.MykhailoOsadtsiv;
+
+import java.util.Arrays;
+
+public class HomeWork5 {
+    /**
+     * Implement the method that returns array of unique elements of input array (filter duplicate elements)
+     * <p>
+     * Examples:
+     * input array - [1, 2, 5, 2, 4]
+     * output array - [1, 2, 5, 4]
+     * <p>
+     * input array - [2, 3, 2, 3]
+     * output array - [2, 3]
+     *
+     //* @param arr - array with elements
+    // * @return - array with elements that don't have duplicates
+     */
+    public static void main(String[] args) {
+        int[] arr1 = new int[]{1, 2, 5, 2, 4};
+        System.out.println(Arrays.toString(uniqueElements(arr1)));
+        int[] arr2 = new int[]{2, 3, 2, 3};
+        System.out.println(Arrays.toString(uniqueElements(arr2)));
+    }
+
+    public static int[] uniqueElements(int[] arr) {
+        int[] output = new int[arr.length];
+        int k = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int x = arr[i];
+            boolean addNew = true;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (x == arr[j]) {
+                    addNew = false;
+                    break;
+                }
+            }
+            if (addNew) {
+                output[k] = x;
+                k++;
+            }
+        }
+        int index = 0;
+        for (int i = 1; i < output.length; i++) {
+            if (output[i] == 0) {
+                index = i;
+                break;
+            }
+        }
+        if (index!=0) {
+            int[] output2 = new int[index];
+            System.arraycopy(output, 0, output2,  0, output2.length);
+            return output2;
+        } else {
+            return output;
+        }
+
+    }
+
+
+    /**
+     * Implement the method that merges two arrays into one. Resulting array should consist of elements that present only in the one array and absent in other
+     * <p>
+     * Example:
+     * arr1 - [1, 2, 4, 5],  arr2 - [1, 3, 7, 5]
+     * output array - [2, 3, 4, 7]
+     *
+     * @param arr1 - first input array with unique elements
+     * @param arr2 - second input array with unique elements
+     * @return array with not common elements from input arrays
+     */
+    public int[] uniqueElementsMergeArray(int[] arr1, int[] arr2) {
+        return null;
+    }
+
+    /**
+     * Implement the method that merges two arrays into one. The resulting array should consist of elements that present in both arrays
+     * <p>
+     * Example:
+     * arr1 - [1, 2, 3] arr2 - [2, 3, 4]
+     * output array - [2, 3]
+     *
+     * @param arr1 - first input array with unique elements
+     * @param arr2 - second input array with unique elements
+     * @return array with common elements from first and second arrays
+     */
+    public int[] commonElements(int[] arr1, int[] arr2) {
+        return null;
+    }
+
+    /**
+     * Implement the method that reverses input array in the opposite order
+     * <p>
+     * Example:
+     * input array - [1, 2, 3, 4]
+     * output array - [4, 3, 2, 1]
+     *
+     * @param arr - input array
+     * @return array with opposite ordering of elements
+     */
+    public int[] reverseArray(int[] arr) {
+        return null;
+    }
+
+    /**
+     * Implement the method that sorts elements of an array that place on even positions  (indexes: 0, 2, 4 ...)
+     * <p>
+     * Example:
+     * input array - [3, 7, 1, 4, 2]
+     * output array - [1, 7, 2, 4, 3]
+     *
+     * @param arr - input array
+     * @return - sorted array of elements on even positions
+     */
+    public int[] sortEvenPositionArray(int[] arr) {
+        return null;
+    }
+}
