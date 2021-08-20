7340729be9b21707e927c1d7da53180a1dfb8d1a
ketrinb <ketrinb@ukr.net>
2021-08-20 08:57:33 +0300

    my Homework 4.

--- src/main/java/aqa/core/lesson4/knenko/HomeWork4.java
+++ src/main/java/aqa/core/lesson4/knenko/HomeWork4.java
@@ -1,8 +1,8 @@
 package aqa.core.lesson4.knenko;
-
+import java.util.Arrays;
 public class HomeWork4 {
         public static void main(String[] args) {
-
+                //task_1
                 int[] myArray = new int[100];
                 int x = 57;
                 for (int i = 0; i < myArray.length; i++) {
@@ -11,22 +11,26 @@ public class HomeWork4 {
                                 System.out.println("Value:" + myArray[i] + ", index:" + i);
                                 break;
                         }
-                        //Unfortunately, the second task did not work.
+                        //task_2. Unfortunately, the second task did not work.
+                        Arrays.sort(myArray);
+
                         int firstIndex = 0;
                         int lastIndex = myArray.length - 1;
 
-                        while (firstIndex != lastIndex) {
+
+                        while (firstIndex <= lastIndex) {
                                 int middleIndex = (firstIndex + lastIndex) / 2;
+                                System.out.println(middleIndex);
 
-                                if (myArray[middleIndex] > x) {
-                                        lastIndex = middleIndex - 1;
+                                if (myArray[middleIndex] > x)
+                                        lastIndex = middleIndex -1;
 
-                                } else if (myArray[middleIndex] < x) {
-                                        firstIndex = middleIndex + 1;
-
-                                } else if (myArray[middleIndex] == x) {
+                                else if (myArray[middleIndex] < x)
+                                        firstIndex = middleIndex +1;
+                                {
                                         System.out.println(middleIndex);
                                 }
+
 
                         }
                 }
