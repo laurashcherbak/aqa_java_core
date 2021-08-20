4b9eaed02402203eabb51b6e0ba45f0aae3f7417
ElenaBeymart <88791850+ElenaBeymart@users.noreply.github.com>
2021-08-20 15:22:44 +0300

    homework3

--- src/main/java/aqa/core/lesson3/elenabeymart/Lesson3.java
+++ src/main/java/aqa/core/lesson3/elenabeymart/Lesson3.java
@@ -1,4 +1,52 @@
 package aqa.core.lesson3.elenabeymart;
 
-public class Lesson3 {
+import java.awt.print.PrinterIOException;
+
+public class Beymart3 {
+    public static main(String[] args){
+        int num = o;
+        System.out.printIn(num);
+        while (num <=20);
+        System.out.printIn(num<=20);
+        if (num % 2 == 0);
+        System.out.printIn(num%2==0);
+    }
 }
+public class Beymart3a
+ int main(){
+     char operator;
+     double first, second;
+     System.out.printIn("Enter the Operator (+,-,*,/:");
+     scanf("%c",&operator);
+     printerf("Enter the two Numbers one by one : ");
+     scanf("%lf %lf", &first,&second);
+
+     switch (operator);
+     {
+         case'+':
+             printf("%2lf - %2lf = %2lf", first,second,(first+second));
+             break;
+         case'-':
+             printf("%2lf - %2lf = %2lf", first,second,(first-second));
+             break;
+         case'*':
+             printf("%2lf * %2lf = %2lf", first,second,(first*second));
+             break;
+         case'-':
+             printf("%2lf - %2lf = %2lf", first,second,(first-second));
+             break;
+         case'/':
+             if(second !=0.0)
+             printf("%2lf / %2lf = %2lf", first,second,(first/second));
+             else
+                 printf("Divide by Zero situation");
+             break;
+         default:("Invalid Operator");
+         break;
+     }
+
+
+
+
+
+ }
