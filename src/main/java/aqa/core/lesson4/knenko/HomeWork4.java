d649f5a80e29a4c843ea8515043360c7e4c2c56f
ketrinb <ketrinb@ukr.net>
2021-08-22 10:11:31 +0300

    my HomeWork 4.

--- src/main/java/aqa/core/lesson4/knenko/HomeWork4.java
+++ src/main/java/aqa/core/lesson4/knenko/HomeWork4.java
@@ -11,23 +11,22 @@ public class HomeWork4 {
                                 System.out.println("Value:" + myArray[i] + ", index:" + i);
                                 break;
                         }
+                }
                         //task_2. Unfortunately, the second task did not work.
                         Arrays.sort(myArray);
 
                         int firstIndex = 0;
                         int lastIndex = myArray.length - 1;
-
+                        int middleIndex = firstIndex + ((lastIndex - firstIndex) / 2);
 
                         while (firstIndex <= lastIndex) {
-                                int middleIndex = (firstIndex + lastIndex) / 2;
-                                System.out.println(middleIndex);
 
-                                if (myArray[middleIndex] > x)
+                                if (myArray[middleIndex] > x) {
                                         lastIndex = middleIndex -1;
 
-                                else if (myArray[middleIndex] < x)
-                                        firstIndex = middleIndex +1;
-                                {
+                                } else if (myArray[middleIndex] < x) {
+                                        firstIndex = middleIndex + 1;
+                                } else if (myArray[middleIndex] == x) {
                                         System.out.println(middleIndex);
                                 }
 
@@ -35,7 +34,6 @@ public class HomeWork4 {
                         }
                 }
         }
-}
 
 
 
