14ddc38b4eb229a3556d9ae0716cbedb6716a281
Vita <viktorya.ukr@gmail.com>
2021-08-23 22:21:05 +0300

    Implement Homework6.

---
+++ src/main/java/aqa/core/lesson6/vriaboshapka/Robot.java
@@ -0,0 +1,19 @@
+package main.java.aqa.core.lesson6.vriaboshapka;
+
+public class Robot extends Child  {
+    String material;
+    public Robot(String material, String name, String sex, int age){
+        this.material=material;
+        this.name=name;
+        this.sex=sex;
+        this.age=age;
+    }
+    @Override
+    public void saySomething(){
+        System.out.println("Beep beep: "+this.name);
+    }
+    public void showMaterial(){
+        System.out.println(this.material);
+    }
+
+}
