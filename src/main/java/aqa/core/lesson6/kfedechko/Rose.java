d93971b836f04fe31c3a8707aa67e848291607f0
kfedechko <f530fa3927adc7263f3b2176090f07eb6f91cc36>
2021-08-24 21:42:30 +0300

    lesson 6 task implemented

---
+++ src/main/java/aqa/core/lesson6/kfedechko/Rose.java
@@ -0,0 +1,14 @@
+package aqa.core.lesson6.kfedechko;
+
+public class Rose extends Flower {
+
+    @Override
+    public boolean isBlooming() {
+        return getAgeDays() > 30 && getAgeDays() < 40;
+    }
+
+    public boolean isRed() {
+        return getColor().equalsIgnoreCase("red");
+    }
+
+}
