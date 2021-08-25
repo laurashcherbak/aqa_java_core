73482f93c07551d356ecb4729c693c8b7bb7e53b
IrynaKrechetova <ikrechetowa@gmail.com>
2021-08-25 08:33:34 +0300

    homework 6 Krechetova

---
+++ src/main/java/aqa/core/lesson6/akylo/IrynaKrechetova/Capital.java
@@ -0,0 +1,25 @@
+package aqa.core.lesson6.akylo.IrynaKrechetova;
+
+public class Capital extends Town {
+
+    private String ParlamentAdress;
+
+    public Capital(String Name, int Population, String ZipCode, String ParlamentAdress) {
+        super(Name, Population, ZipCode);
+        this.ParlamentAdress = ParlamentAdress;
+    }
+
+    @Override
+    public String toString () {
+        return this.getName() + " is a Capital";
+    }
+
+    public String getParlamentAdress () {
+        return ParlamentAdress;
+    }
+
+    public void setParlamentAdress (String newAdress) {
+        ParlamentAdress = newAdress;
+    }
+
+}
---
+++ src/main/java/aqa/core/lesson6/akylo/IrynaKrechetova/Town.java
@@ -0,0 +1,93 @@
+package aqa.core.lesson6.akylo.IrynaKrechetova;
+
+
+public class Town {
+    private String Name;
+    private int Population;
+    private String ZipCode;
+
+    public void setName(String newName)
+    {
+        this.Name = newName;
+    }
+
+    public String getName() {
+        return this.Name;
+    }
+
+    public boolean IsMame (String newName) {
+        return newName.equals(this.Name);
+    }
+
+    public void AddPeople (int newPeople) {
+        this.Population = this.Population + newPeople;
+    }
+
+    public Town (String Name, int Population, String ZipCode) {
+        this.Name = Name;
+        this.Population = Population;
+        this.ZipCode = ZipCode;
+    }
+
+    public Town (String Name, int Population) {
+        this.Name = Name;
+        this.Population = Population;
+        this.ZipCode = "00000";
+    }
+
+    public Town (String Name) {
+        this.Name = Name;
+        this.Population = 0;
+        this.ZipCode = "00000";
+    }
+
+    public Town () {
+        this.Name = "no name";
+        this.Population = 0;
+        this.ZipCode = "00000";
+    }
+
+    @Override
+    public String toString () {
+        return this.Name;
+    }
+
+    @Override
+    public boolean equals (Object otherTown) {
+        if (this == otherTown) return true;
+
+        if (otherTown == null || getClass() != otherTown.getClass())
+            return false;
+
+        Town town = (Town) otherTown;
+        if (!this.Name.equals(town.Name)){
+            return false;
+        }
+
+        if (this.Population != town.Population)
+            return false;
+
+        if (!this.ZipCode.equals(town.ZipCode)) {
+            return false;
+        }
+
+        return true;
+
+    }
+
+    @Override
+    public int hashCode() {
+
+        int result = this.Population;
+        if (this.Name != null) {
+            result = result + this.Name.hashCode();
+        }
+        if (this.ZipCode != null) {
+            result = result + this.ZipCode.hashCode();
+        }
+        return result;
+    }
+
+}
+
+
