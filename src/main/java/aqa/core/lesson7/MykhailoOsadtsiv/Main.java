a5bc5892b5850a08d63a926042f961f48eea7072
OsadtsivMykhailo <osadtsiv.m@gmail.com>
2021-08-25 20:53:40 +0300

    HomeWork 7 //task 4

--- src/main/java/aqa/core/lesson7/MykhailoOsadtsiv/Main.java
+++ src/main/java/aqa/core/lesson7/MykhailoOsadtsiv/Main.java
@@ -12,5 +12,9 @@ public class Main {
         Rectange re1 = new Rectange("RED", 11, 22);
         System.out.println(re1.toString());
         System.out.println("Shape area is : " + String.format("%.0f",re1.calcArea()));
+
+        Triangle tr1 = new Triangle("BLACK", 5, 5, 5);
+        System.out.println(tr1.toString());
+        System.out.println("Shape area is : " + String.format("%.3f",tr1.calcArea()));
     }
 }
