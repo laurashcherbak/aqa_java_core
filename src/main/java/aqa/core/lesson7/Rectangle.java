6b188cb506ebfd5ba036e8f3e7d4377d5134d0f1
Vlasiuk Oksana <ksenia.vlasiuk7@gmail.com>
2021-08-26 10:07:20 +0300

    lesson 7 Vlasiuk Oksana

---
+++ src/main/java/aqa/core/lesson7/Rectangle.java
@@ -0,0 +1,23 @@
+package aqa.core.lesson7;
+
+//Task3
+public class Rectangle extends Shape {
+    private double width;
+    private double height;
+    public Rectangle(String color, double width, double height) {
+        super(color);
+        this.name = "Rectangle";
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double calcArea() {
+        return width * height;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", width=" + this.width + ", height=" + this.height;
+    }
+}
