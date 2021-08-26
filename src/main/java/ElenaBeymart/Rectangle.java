f19151ce7615eff80b8556b4e4cc5fe4303068fa
ElenaBeymart <88791850+ElenaBeymart@users.noreply.github.com>
2021-08-26 09:46:38 +0300

    homework6

---
+++ src/main/java/ElenaBeymart/Rectangle.java
@@ -0,0 +1,8 @@
+package ElenaBeymart;
+
+public class Rectangle extends Shape {
+    //Calculate and return area of triangle
+    public double getArea() {
+        return (getHeight() * getWidth()) / 2;
+    }
+}
---
+++ src/main/java/ElenaBeymart/Triangle.java
@@ -0,0 +1,8 @@
+package ElenaBeymart;
+
+public class Triangle extends Shape {
+    //Calculate and return area of triangle
+    public double getArea() {
+        return (getHeight() * getWidth()) / 2;
+    }
+}
