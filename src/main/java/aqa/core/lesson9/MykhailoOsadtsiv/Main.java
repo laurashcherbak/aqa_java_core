a62eefad7f072fdaa4c5803ac535ab9cd19ff941
OsadtsivMykhailo <osadtsiv.m@gmail.com>
2021-08-27 11:54:35 +0300

    HomeWork 9 // Task 2

--- src/main/java/aqa/core/lesson9/MykhailoOsadtsiv/Main.java
+++ src/main/java/aqa/core/lesson9/MykhailoOsadtsiv/Main.java
@@ -8,6 +8,11 @@ public class Main {
         System.out.println(myStr.substring(3, 6));
         System.out.println(reverseString("abcd"));
         System.out.println("//////////////////////////////////////");
+
+        String myStr1 = "Cartoon";
+        String myStr2 = "Tomcat";
+
+        firstNotSecond(myStr1, myStr2);
     }
 
     public static String reverseString (String input) {
@@ -16,5 +21,13 @@ public class Main {
             output += input.charAt(i);
         }
         return output;
+    }
+
+    public static void firstNotSecond (String word1, String word2) {
+        for (int i = word1.length() - 1; i >= 0; i--) {  //порядок виводу в завданні не вказаний
+            if (word2.indexOf(word1.charAt(i)) == -1) {
+                System.out.println(word1.charAt(i));
+            }
+        }
     }
 }
