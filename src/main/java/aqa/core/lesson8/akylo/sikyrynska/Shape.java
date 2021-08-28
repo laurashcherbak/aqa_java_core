a06848fdbdd0552c3e0a62e4776253491a8b4ced
KvitoslavaSikyrynska <88768200+KvitoslavaSikyrynska@users.noreply.github.com>
2021-08-28 15:20:56 +0300

    Homework8. Abstract classes and Interfaces.

---
+++ src/main/java/aqa/core/lesson8/akylo/sikyrynska/Shape.java
@@ -0,0 +1,49 @@
+package aqa.core.lesson8.akylo.sikyrynska;
+import java.util.Comparator;
+
+abstract class Shape implements Drawable, Comparable {
+    private String sC;
+
+    public Shape() {
+        this.sC = " ";
+    }
+
+    public Shape(String sC) {
+        this.sC = sC;
+    }
+
+    public String getShapeColor() {
+        return sC;
+    }
+
+    public void setShapeColor(String sC) {
+        this.sC = sC;
+    }
+
+
+    public abstract double calcArea();
+
+    @Override
+    public String toString() {
+        return "This is shape, colour is " + sC;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println(toString()+", area is "+ calcArea());
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Shape s = (Shape)o;
+        if(this.calcArea() < s.calcArea()) {
+            return -1;
+        }if(this.calcArea() > s.calcArea()) {
+            return 1;
+        }
+        return 0;
+    }
+
+
+
+}
