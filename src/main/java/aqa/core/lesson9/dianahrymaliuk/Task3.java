e49f4aa0303e20844da89c72c5400bcff0e7034d
DianaHrymaliuk <diana.hrymaliuk@gmail.com>
2021-08-29 01:50:10 +0300

    HomeWork9

---
+++ src/main/java/aqa/core/lesson9/dianahrymaliuk/Task3.java
@@ -0,0 +1,41 @@
+package aqa.core.lesson9.dianahrymaliuk;
+
+
+import java.util.Arrays;
+
+public class Task3 {
+    public static void main(String []args){
+        String line = "Using methods of class String";
+        char[] str2 = line.toCharArray();
+        for (int i = 1; i < str2.length; i++) {
+//
+//        for (int i = 0; i < chars.length; i++) {
+//            for (int j = i + 1; j < chars.length; j++) {
+//                if (chars[i] == ' ') {
+//                    continue;
+//                }
+//                if (chars[i] == chars[j]) {
+//                    chars[j] = 0;
+//                }
+//            }
+      System.out.println(Arrays.toString());
+                }}
+//    }
+//    public static String uniqueElements(String str1) {
+//        char[] str2 = str1.toCharArray();
+//        if (str2 == null || str2.length == 0)
+//        Arrays.sort(str2);
+//        int n = 1;
+//        for (int i = 1; i < str2.length; i++) {
+//            if (str2[i] != str2[i - 1]) n++;
+//        }
+//        int[] res = new int[n];
+//        res[0] = str2[0];
+//        n = 1;
+//        for (int i = 1; i < str2.length; i++) {
+//            if (str2[i] != str2[i - 1]) res[n++] = str2[i];
+//        }
+//        return new String(String.valueOf(res));
+//    }
+
+
