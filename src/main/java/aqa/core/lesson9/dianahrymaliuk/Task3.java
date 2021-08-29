7974d66d36ade7b03d3066a952d3f10d923ff58d
DianaHrymaliuk <diana.hrymaliuk@gmail.com>
2021-08-29 22:26:24 +0300

    HomeWork9

--- src/main/java/aqa/core/lesson9/dianahrymaliuk/Task3.java
+++ src/main/java/aqa/core/lesson9/dianahrymaliuk/Task3.java
@@ -1,20 +1,21 @@
 package aqa.core.lesson9.dianahrymaliuk;
 
 
-import java.util.*;
 
 public class Task3 extends Task2 {
-    public static void main(String []args){
-        String line = "Using methods of class String";
-        char[] myArray = line.toCharArray();
-        Arrays.sort(myArray);
-        for (int i = 0; i < myArray.length; i++) {
-            for (int j = i + 1; j < myArray.length; j++) {
-                if (myArray[i] == myArray[j]) {
-                    myArray[i] = line.charAt(i);
-                }
+    public static void main (String [] args) {
+        System.out.println(uniqueChars("Using methods of class String"));
+    }
+    public static char[] uniqueChars (String str) {
+        StringBuilder uniq = new StringBuilder();
+        for (int i= 0; i < str.length(); i++){
+            char element = str.charAt(i);
+            if (str.indexOf(element) == str.lastIndexOf(element)) {
+                uniq.append(element);
             }
+        }
+        return uniq.toString().toCharArray();
     }
 }
-}
+
 
