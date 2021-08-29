ae8e523fbadf524ecf184041309d294fec2ca272
Назар <hakernaz@gmail.com>
2021-08-29 17:55:42 +0300

    HomeWork8

---
+++ src/main/java/aqa/core/lesson8/akylo/NazarVdovych8/Shape.java
@@ -0,0 +1,39 @@
+package aqa.core.lesson8.akylo.NazarVdovych8;
+
+public class Shape {
+    public Shape(String shapeColor) {
+        abstract class Shape implements Drawable, Comparable<Shape> {
+            private final String shapeColor;
+
+            public Shape(String shapeColor) {
+                this.shapeColor = shapeColor;
+            }
+
+            @Override
+            public void draw() {
+                System.out.println(this + ", area is: " + calcArea());
+            }
+
+            @Override
+            public int compareTo(Shape other) {
+
+                double thisArea = calcArea();
+                double otherArea = other.calcArea();
+                if (thisArea == otherArea) {
+                    return 0;
+                }
+                if (thisArea < otherArea) {
+                    return -1;
+                }
+                return 1;
+            }
+
+            @Override
+            public String toString() {
+                return "This is " + getClass().getSimpleName() + ", color is: " + shapeColor;
+            }
+
+            abstract double calcArea();
+        }
+    }
+}
