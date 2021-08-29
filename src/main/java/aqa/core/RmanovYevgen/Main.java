05d015376d3397a556a06651a9d80d3e48b6242e
Jegoush <l.jegoush@gmail.com>
2021-08-29 21:29:12 +0300

    RomanovYevgen_HomeWork9

---
+++ src/main/java/aqa/core/RmanovYevgen/Main.java
@@ -0,0 +1,21 @@
+package aqa.core.RmanovYevgen;
+
+public class Main {
+    public static void main(String[] args) {
+        String myStr = "Romanov";
+        System.out.println(myStr.indexOf("a"));
+        System.out.println(myStr.lastIndexOf("b"));
+        System.out.println(myStr.substring(1, 2));
+        System.out.println(reverseString("Romanov"));
+        System.out.println("Yevgen");
+    }
+
+    public static String reverseString (String input) {
+        String output = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            output += input.charAt(i);
+        }
+        final String output1 = output;
+        return output1;
+    }
+}
