d058a6f979319e93833e444c2a18da17e8403e82
PavloTymofiiuk <pavlo.tymofiiuk@gmail.com>
2021-08-30 01:51:54 +0300

    Created 8 hometask.

---
+++ src/main/java/aqa/core/lesson8/Tymofiiuk/Rectangle.java
@@ -0,0 +1,23 @@
+package aqa.core.lesson8;
+
+public class Rectangle extends Shape {
+    private final double a;
+    private final double b;
+
+
+    public Rectangle(String shapeColor, String shapeName, double a, double b) {
+        super(shapeColor, shapeName);
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle" + " a=" + a +" b=" + b;
+    }
+
+    public double calcArea() {
+        return (a * b);
+
+    }
+}
---
+++ src/main/java/aqa/core/lesson8/Tymofiiuk/Triangle.java
@@ -0,0 +1,24 @@
+package aqa.core.lesson8;
+
+public class Triangle extends Shape {
+    private final double a;
+    private final double b;
+    private final double c;
+
+    public Triangle(String shapeColor, String shapeName, double a, double b, double c) {
+        super(shapeColor, shapeName);
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle" + " a=" + a +" b=" + b + " c=" + c;
+    }
+
+    public double calcArea() {
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * ((p - a) * (p - b) * (p - c)));
+    }
+}
