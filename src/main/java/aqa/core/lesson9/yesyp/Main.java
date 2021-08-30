6b34170ae18e963417ef8465dfd9113776be2a02
olich <olichka007@gmail.com>
2021-08-30 16:00:50 +0300

    Lesson9 String progressing

---
+++ src/main/java/aqa/core/lesson9/yesyp/Main.java
@@ -0,0 +1,18 @@
+package aqa.core.lesson9.yesyp;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String myStr = "My best education project in GlobalLogic";
+        int i1 = myStr.indexOf("a");
+        int i2 = myStr.lastIndexOf("b");
+        System.out.println("First “a”: " + i1);
+        System.out.println("Last “b”: " + i2);
+        String subString = myStr.substring(2, 6);
+        System.out.println("From 3 to 6 char index:" + subString);
+        System.out.println("Reverse string: " + Reverse.reverseString("My best education project in GlobalLogic"));
+        }
+    }
+
+
+
