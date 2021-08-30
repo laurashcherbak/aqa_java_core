17558af3fe8e08d8d95de399ceedce4c5e62637f
andrii.kylo <andrii.kylo@groupm.tech>
2021-08-30 15:58:42 +0300

    refactored

--- src/main/java/aqa/core/lesson8/irynaKrechetova/Main.java
+++ src/main/java/aqa/core/lesson8/irynaKrechetova/Main.java
@@ -16,10 +16,10 @@ public class Main {
         System.out.print("Shape area is: ");
         System.out.print(circle1.calcArea());
 
-        Rectangle rectangle1 = new Rectangle("PURPLE", 11, 22);
-        System.out.println(rectangle1.toString());
-        System.out.print("Shape area is: ");
-        System.out.println(rectangle1.calcArea());
+//        Rectangle rectangle1 = new Rectangle("PURPLE", 11, 22);
+//        System.out.println(rectangle1.toString());
+//        System.out.print("Shape area is: ");
+//        System.out.println(rectangle1.calcArea());
 
         Triangle triangle1 = new Triangle("Grey", 5, 5, 5);
         System.out.println(triangle1.toString());
@@ -27,38 +27,38 @@ public class Main {
         System.out.println(triangle1.calcArea());
 
 
-        Rectangle rectangle2 = new Rectangle("orange", 2, 2);
-        Rectangle rectangle3 = new Rectangle("orange", 3, 3);
-        Rectangle rectangle4 = new Rectangle("orange", 4, 4);
-        Rectangle rectangle5 = new Rectangle("orange", 5, 5);
-        Rectangle rectangle6 = new Rectangle("orange", 6, 6);
+//        Rectangle rectangle2 = new Rectangle("orange", 2, 2);
+//        Rectangle rectangle3 = new Rectangle("orange", 3, 3);
+//        Rectangle rectangle4 = new Rectangle("orange", 4, 4);
+//        Rectangle rectangle5 = new Rectangle("orange", 5, 5);
+//        Rectangle rectangle6 = new Rectangle("orange", 6, 6);
         Circle circle2 = new Circle("pink", 5);
         Circle circle3 = new Circle("pink", 6);
         Triangle triangle2 = new Triangle("yellow", 1, 1, 1);
         Triangle triangle3 = new Triangle("yellow", 2, 2, 2);
 
-        Shape[] arr = new Shape[]{rectangle2, rectangle3, rectangle4, rectangle5, rectangle6, circle2, circle3, triangle2, triangle3};
+//        Shape[] arr = new Shape[]{rectangle2, rectangle3, rectangle4, rectangle5, rectangle6, circle2, circle3, triangle2, triangle3};
 
         double sumArea = 0;
         double sumCircleArea = 0;
         double sumRectangleArea = 0;
         double sumTriangleArea = 0;
 
-        for (Shape x: arr) {
-            System.out.println(x.toString());
-            System.out.println("Shape area is: ");
-            System.out.print(x.calcArea());
-            sumArea = sumArea + x.calcArea();
-            if (x instanceof Circle) {
-                sumCircleArea = sumCircleArea + x.calcArea();
-            }
-            if (x instanceof Triangle) {
-                sumTriangleArea = sumTriangleArea + x.calcArea();
-            }
-            if (x instanceof Rectangle) {
-                sumRectangleArea = sumRectangleArea + x.calcArea();
-            }
-        }
+//        for (Shape x: arr) {
+//            System.out.println(x.toString());
+//            System.out.println("Shape area is: ");
+//            System.out.print(x.calcArea());
+//            sumArea = sumArea + x.calcArea();
+//            if (x instanceof Circle) {
+//                sumCircleArea = sumCircleArea + x.calcArea();
+//            }
+//            if (x instanceof Triangle) {
+//                sumTriangleArea = sumTriangleArea + x.calcArea();
+//            }
+//            if (x instanceof Rectangle) {
+//                sumRectangleArea = sumRectangleArea + x.calcArea();
+//            }
+//        }
 
         System.out.println("Total area = ");
         System.out.println(sumArea);
@@ -72,7 +72,7 @@ public class Main {
         System.out.println("Total area of Rectangles = ");
         System.out.println(sumRectangleArea);
 
-        for (Shape x : arr) {x.draw();}
+//        for (Shape x : arr) {x.draw();}
         }
 
     }
