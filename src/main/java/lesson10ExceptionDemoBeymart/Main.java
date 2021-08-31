90534d5c91565f31569d038ff4ead6ecb365e561
ElenaBeymart <88791850+ElenaBeymart@users.noreply.github.com>
2021-08-31 14:21:48 +0300

    homework10

---
+++ src/main/java/lesson10ExceptionDemoBeymart/Main.java
@@ -0,0 +1,14 @@
+package lesson10ExceptionDemoBeymart;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Third");
+        Person one = new Person();
+        try {
+            one.setAge(0);
+        } catch (Exception msg) {
+            System.out.println("ExceptionDemo.main - ErrorAge");
+            System.out.println("msg");
+        }
+    }
+}
