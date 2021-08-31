ee3f7369ccacd106847be15bb4e7b92b493bf38a
ElenaBeymart <88791850+ElenaBeymart@users.noreply.github.com>
2021-08-31 13:33:59 +0300

    homework10

--- src/main/java/lesson10elenabeymart/MyException.java
+++ src/main/java/lesson10elenabeymart/MyException.java
@@ -2,7 +2,14 @@ package lesson10elenabeymart;
 
 public class MyException extends FirstException {
 
+    private String msg;
+
     MyException(String msg) {
         super(msg);
+        this.msg = msg;
     }
-}
+        public String print() {
+            return msg;
+        }
+    }
+
