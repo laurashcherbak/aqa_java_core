890cf8a10b0e3e8b9a2c05d7bda35f7bd0c0256e
nataskrypak <zakaznataiua@gmail.com>
2021-09-02 17:45:27 +0300

    lesson 11

---
+++ src/main/java/aqa/core/nSkrypak/CompanyMain.java
@@ -0,0 +1,21 @@
+package aqa.core.nSkrypak;
+
+public class CompanyMain {
+    private String name;
+    private int telephone;
+    private String address;
+    String[] employee;
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = "Company!";}
+
+    public int getTelephone() { return telephone; }
+    public void setTelephone(int telephone) { this.telephone = 80965664455; }
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = "Lviv, 35 Kopernyka str"; }
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/aqa/core/nSkrypak/Employees.java
@@ -0,0 +1,8 @@
+package aqa.core.nSkrypak;
+
+public class Employees {
+    String employeesName;
+    int employeesPhone;
+    double salary;
+    Employees employees = new Employees();
+}
