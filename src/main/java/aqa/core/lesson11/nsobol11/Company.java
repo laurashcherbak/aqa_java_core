a79959bd751de2d097d7094a86aa55299da0780f
andrii.kylo <andrii.kylo@groupm.tech>
2021-09-03 16:05:39 +0300

    commented broken code

--- src/main/java/aqa/core/lesson11/nsobol11/Company.java
+++ src/main/java/aqa/core/lesson11/nsobol11/Company.java
@@ -16,54 +16,54 @@ public class Company {
             //this.emp=emp;
         }
         //task1
-        public String getMaxSalary(){
-            int maxSalary=0;
-            String name="";
-            for(int i=0; i< emp.length;i++){
-                if (emp[i].getSalary() >= maxSalary) {
-                    maxSalary = emp[i].getSalary();
-                    name = emp[i].getName();
-                }
-            }return name;
-        }
-        //task2
-        public void addNew(Employee person) throws Exception{
+//        public String getMaxSalary() {
+//            int maxSalary = 0;
+//            String name = "";
+//            for(int i=0; i< emp.length;i++){
+//                if (emp[i].getSalary() >= maxSalary) {
+//                    maxSalary = emp[i].getSalary();
+//                    name = emp[i].getName();
+//                }
+//            }return name;
+//        }
+            //task2
+//        public void addNew(Employee person) throws Exception{
 //task6
-            if(minSalary> person.getSalary()){
-                throw new Exception("Salary is too little");
-            }
-            Employee arr=new Employee[emp.length+1];
-            for (int i=0;i<emp.length; i++){
-                arr[i]=emp[i];
-            }
-            arr[emp.length+1]=person;
-            this.emp=arr;
-        }
-        //task3
-        public void fireOne(Employee bad){
-            Employee arr1=new Employee[emp.length-1];
-            int index=0;
-            for (int i=0;i<emp.length; i++){
-                if(bad != emp[i]){
-                    arr1[index]=emp[i];
-                    index++;
-                }
-            }
-            this.emp=arr1;
-        }
+//            if(minSalary> person.getSalary()){
+//                throw new Exception("Salary is too little");
+//            }
+//            Employee arr=new Employee[emp.length+1];
+//            for (int i=0;i<emp.length; i++){
+//                arr[i]=emp[i];
+//            }
+//            arr[emp.length+1]=person;
+//            this.emp=arr;
+//        }
+            //task3
+//            public void fireOne (Employee bad){
+//            Employee arr1=new Employee[emp.length-1];
+//            int index=0;
+//            for (int i=0;i<emp.length; i++){
+//                if(bad != emp[i]){
+//                    arr1[index]=emp[i];
+//                    index++;
+//                }
+//            }
+//            this.emp=arr1;
+//        }
 
-        //task4
-        public int getExpenses(){
-            int expenses =0;
+                //task4
+//        public int getExpenses(){
+//            int expenses =0;
 
-            for(int i=0; i<emp.length;i++){
-                expenses+=emp[i].getSalary();
+//            for(int i=0; i<emp.length;i++){
+//                expenses+=emp[i].getSalary();
+//            }
+//            return expenses;
+//        }
+                //task5
+//        public int avgSalary(){
+//            return getExpenses()/ emp.length;
+//        }
+
             }
-            return expenses;
-        }
-        //task5
-        public int avgSalary(){
-            return getExpenses()/ emp.length;
-        }
-
-    }
--- src/main/java/aqa/core/lesson11/nsobol11/Employee.java
+++ src/main/java/aqa/core/lesson11/nsobol11/Employee.java
@@ -6,33 +6,33 @@ public class Employee {
         int phone;
         int salary;
 
-        public Employee(String name,; int phone, int salary) {
-        this.name = name;
-        this.phone = phone;
-        this.salary = salary;
+//        public Employee(String name,; int phone, int salary) {
+//        this.name = name;
+//        this.phone = phone;
+//        this.salary = salary;
     }
 
-        public String getName () {
-        return name;
-    }
+//        public String getName () {
+//        return name;
+//    }
         public void setName (String name){
-        this.name = name;
+//        this.name = name;
     }
-        public int getPhone () {
-        return phone;
-    }
+//        public int getPhone () {
+//        return phone;
+//    }
 
         public void setPhone ( int phone){
-        this.phone = phone;
+//        this.phone = phone;
     }
 
-        public int getSalary () {
-        return salary;
-    }
+//        public int getSalary () {
+//        return salary;
+//    }
 
         public void setSalary ( int salary){
-        this.salary = salary;
+//        this.salary = salary;
     }
-    }
+//    }
 }
 
