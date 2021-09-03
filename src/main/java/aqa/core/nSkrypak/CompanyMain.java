f68bc289ab4811f93878974329b4fa2350aa468a
nataskrypak <zakaznataiua@gmail.com>
2021-09-03 17:01:39 +0300

    lesson 11 continue

--- src/main/java/aqa/core/nSkrypak/CompanyMain.java
+++ src/main/java/aqa/core/nSkrypak/CompanyMain.java
@@ -4,18 +4,27 @@ public class CompanyMain {
     private String name;
     private int telephone;
     private String address;
-    String[] employee;
+    Employees[] employee = new Employees[5];
 
     public String getName() { return name; }
     public void setName(String name) { this.name = "Company!";}
-
     public int getTelephone() { return telephone; }
-    public void setTelephone(int telephone) { this.telephone = 80965664455; }
+    public void setTelephone(int telephone) { this.telephone = 965664455; }
     public String getAddress() { return address; }
     public void setAddress(String address) { this.address = "Lviv, 35 Kopernyka str"; }
 
-
     public static void main(String[] args) {
-
+        Employees employee1 = new Employees("Diana", 987657657, 6000.35);
+        System.out.println(employee1);
+        Employees employee2 = new Employees("Olga", 667890900, 12000.55);
+        System.out.println(employee2);
+        Employees employee3 = new Employees("Ivan", 677845901, 45000.55);
+        System.out.println(employee3);
+        Employees employee4 = new Employees("Oksana", 680000900, 4000.55);
+        System.out.println(employee4);
+        Employees employee5 = new Employees("Olga", 967890933, 18000.55);
+        System.out.println(employee5);
     }
+
+
 }
--- src/main/java/aqa/core/nSkrypak/Employees.java
+++ src/main/java/aqa/core/nSkrypak/Employees.java
@@ -4,5 +4,21 @@ public class Employees {
     String employeesName;
     int employeesPhone;
     double salary;
-    Employees employees = new Employees();
+
+
+    public String getEmployeesName() { return employeesName; }
+    public int getEmployeesPhone() { return employeesPhone; }
+    public double getSalary() { return salary; }
+
+    public void setEmployeesName(String employeesName) {this.employeesName = employeesName;}
+    public void setEmployeesPhone(int employeesPhone) {this.employeesPhone = employeesPhone;}
+    public void setSalary(double salary) {this.salary = salary;}
+
+    public Employees(String employeesName, int employeesPhone, double salary) {
+        this.employeesName = employeesName;
+        this.employeesPhone = employeesPhone;
+        this.salary = salary;
+    }
+
+
 }
